dc98935580a8ff116d9e5e0bfb94e6c91af69668
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-02-29 12:33:08 -0300

    base files

---
+++ src/main/java/com/aguslxrd/realtimechat/RealtimechatApplication.java
@@ -0,0 +1,13 @@
+package com.aguslxrd.realtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aguslxrd/realtimechat/RealtimechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguslxrd.realtimechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
